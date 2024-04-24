07577f1dd5bdf74a349fd6e6cc01b328b483fb67
Vasco Ceti <vasco.ceti@gmail.com>
2024-04-24 09:46:44 +0200

    custom renderers in react markdown

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,24 +1,32 @@
+import Image from "next/image";
 import classes from "./post-content.module.css";
 import PostHeader from "./post-header";
 import ReactMarkdown from "react-markdown";
-
-const DUMMY_POST = {
-  slug: "getting-started-with-nextjs",
-  title: "Getting Started with NextJS",
-  image: "getting-started-nextjs.png",
-  date: "2022-02-10",
-  content:
-    "# This is a first post\n\nAnd this is the content of the first post!",
-};
 
 export default function PostContent(props) {
   const { post } = props;
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
+  const customRenderers = {
+    image(image) {
+      return (
+        <>
+          {" "}
+          <Image
+            src={`/images/posts/${post.slug}/${image.src}`}
+            alt={image.alt}
+            width={600}
+            height={300}
+          />{" "}
+        </>
+      );
+    },
+  };
+
   return (
     <article className={classes.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
     </article>
   );
 }
